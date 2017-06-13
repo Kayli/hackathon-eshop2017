66ffa0ac60c867bc912605e479e9915bdf68f519
ikarpenkov <ikarpenkov@pnimedia.com>
2017-06-13 12:01:08 -0700

    added example of constructor injection for order controller

--- PNI.EShop.UI/Controllers/OrderController.cs
+++ PNI.EShop.UI/Controllers/OrderController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using PNI.EShop.Core;
 
 // For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
@@ -10,6 +11,15 @@ namespace PNI.EShop.UI.Controllers
 {
     public class OrderController : Controller
     {
+        private readonly IOrderService _orderService;
+
+        public OrderController(IOrderService orderService)
+        {
+            _orderService = orderService;
+        }
+        
+        
+        
         // GET: /<controller>/
         public IActionResult Index()
         {
