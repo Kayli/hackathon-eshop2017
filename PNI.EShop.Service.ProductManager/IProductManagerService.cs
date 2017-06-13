405ca1a339c9ddee9e50a19fa9c5edfc910bb155
Jeff Vanzella <jvanzella@pnimedia.com>
2017-06-13 15:20:53 -0700

    Add ProductManager Service

---
+++ PNI.EShop.Service.ProductManager/IProductManagerService.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+
+namespace PNI.EShop.Service.ProductManager
+{
+    public interface IProductManagerService
+    {
+        IEnumerable<Product> RetrieveAllProducts();
+    }
+}
