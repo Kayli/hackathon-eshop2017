5869d584e174f202868711b19c5e0fa9577ba750
Jeff Vanzella <jvanzella@pnimedia.com>
2017-06-13 11:42:41 -0700

    Add basic Products Page with Static Constant products

--- PNI.EShop.UI/Controllers/ProductsController.cs
+++ PNI.EShop.UI/Controllers/ProductsController.cs
@@ -1,12 +1,20 @@
 using Microsoft.AspNetCore.Mvc;
+using PNI.EShop.UI.Services;
 
 namespace PNI.EShop.UI.Controllers
 {
     public class ProductsController : Controller
     {
+        private readonly IProductsService _productsService;
+
+        public ProductsController(IProductsService productsService)
+        {
+            _productsService = productsService;
+        }
+
         public IActionResult Index()
         {
-            return View();
+            return View(_productsService.ListOfAllProducts());
         }
     }
 }
