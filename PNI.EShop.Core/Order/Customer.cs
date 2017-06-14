dd30269f3807363e7fa5537d9063165c1e71a6d6
heleng <hgerasymenko@pnimedia.com>
2017-06-14 16:26:11 -0700

    changed the validation messages for order form

--- PNI.EShop.Core/Order/Customer.cs
+++ PNI.EShop.Core/Order/Customer.cs
@@ -9,16 +9,16 @@ namespace PNI.EShop.Core.Order
 {
     public class Customer
     {
-        [Required(ErrorMessage = "First Name Required")]
-        [StringLength(50, ErrorMessage = "Name too long, must be under 50 characters")]
+        [Required(ErrorMessage = "First name is required")]
+        [StringLength(50, ErrorMessage = "Name is too long, must be under 50 characters")]
         public string FirstName { get; set; }
-        [Required(ErrorMessage = "Last Name Required")]
-        [StringLength(50, ErrorMessage = "Name too long, must be under 50 characters")]
+        [Required(ErrorMessage = "Last name is required")]
+        [StringLength(50, ErrorMessage = "Name is too long, must be under 50 characters")]
         public string LastName { get; set; }
-        [Required(ErrorMessage = "Address Name Required")]
-        [StringLength(200, ErrorMessage = "Address too long, must be under 200 characters")]
+        [Required(ErrorMessage = "Address is required")]
+        [StringLength(200, ErrorMessage = "Address is too long, must be under 200 characters")]
         public string Address { get; set; }
-        [Required(ErrorMessage = "PostalCode Name Required")]
+        [Required(ErrorMessage = "Postal code is required")]
         [StringLength(6, ErrorMessage = "A Canadian postal code is 6 characters!")]
         [RegularExpression("^([a-zA-Z][0-9]){3}$", ErrorMessage = "Not a valid Canadian Postal Code")]
         public string PostalCode { get; set; }
