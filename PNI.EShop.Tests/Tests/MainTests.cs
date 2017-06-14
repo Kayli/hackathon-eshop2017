8185c6d2d45a4db4d6a7859789e37b3f91643d0c
heleng <hgerasymenko@pnimedia.com>
2017-06-14 10:25:53 -0700

    updated a test

--- PNI.EShop.Tests/Tests/MainTests.cs
+++ PNI.EShop.Tests/Tests/MainTests.cs
@@ -10,6 +10,11 @@ namespace PNI.EShop.Tests
     {
         public ProductsPage _productsPage;
 
+        public override void BeforeAll()
+        {
+            base.BeforeAll();
+            _productsPage = new ProductsPage(_driver);
+        }
 
        [Test]
        public void Can_view_products_list()
