a430c90956f27a8160757c9a8a52718eaa7e03eb
heleng <hgerasymenko@pnimedia.com>
2017-06-14 09:57:32 -0700

    added Can_view_products_list

---
+++ PNI.EShop.Tests/Pages/ProductsPage.cs
@@ -0,0 +1,18 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Support.PageObjects;
+using PNI.EShop.Tests.Base;
+
+namespace PNI.EShop.Tests.Pages
+{
+    public class ProductsPage : PageBase
+    {
+        public ProductsPage(IWebDriver driver) : base(driver)
+        {
+        }
+
+        public int GetProductsCount => _driver.FindElements(By.XPath("//table//a")).Count;
+
+        [FindsBy(How = How.XPath, Using = "(//table//a)[1]")]
+        internal IWebElement FirstProduct;
+    }
+}
